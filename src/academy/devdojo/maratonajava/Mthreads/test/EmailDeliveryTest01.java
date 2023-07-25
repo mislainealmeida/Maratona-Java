d45aef548f6cfbd8d0265f9f12c877173b57ff13
Mislaine <a.s.mislaine@gmail.com>
2023-07-25 09:55:05 -0300

    threads

---
+++ src/academy/devdojo/maratonajava/Mthreads/test/EmailDeliveryTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.Mthreads.test;
+
+import academy.devdojo.maratonajava.Mthreads.domonio.Members;
+import academy.devdojo.maratonajava.Mthreads.service.EmailDeliveryService;
+
+import javax.swing.*;
+
+public class EmailDeliveryTest01 {
+    public static void main(String[] args) {
+        Members members = new Members();
+        Thread jiraya = new Thread(new EmailDeliveryService(members), "Jiraya");
+        Thread kakashi = new Thread(new EmailDeliveryService(members), "Kakashi");
+        jiraya.start();
+        kakashi.start();
+        while(true){
+            String email = JOptionPane.showInputDialog("Entre com seu email");
+            if(email == null || email.isEmpty()){
+                members.close();
+                break;
+            }
+            members.addMemberEmail(email);
+        }
+    }
+}
